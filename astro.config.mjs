0e95aba087c373fcb637b5f26554bd432d991c16
Josh Rehders <jr8ers@gmail.com>
2025-03-11 17:10:12 -0400

    with cloudinary?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rehde.rs",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  })]
+  integrations: [
+    tailwind(), 
+    // Remove the image integration or keep it only for local images
+    mdx(), 
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ]
 });

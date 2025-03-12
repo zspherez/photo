493e22e2e6ccf39f1ba349224dc000db3df36582
Josh Rehders <jr8ers@gmail.com>
2025-03-11 23:52:18 -0400

    add headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,23 +9,10 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: "https://rehde.rs",
   integrations: [tailwind(),
-  // Remove the image integration or keep it only for local images
   mdx(), partytown({
     config: {
       debug: true,
       forward: ['dataLayer.push', 'gtag']
     }
-  })],
-  output: "server",
-  adapter: cloudflare({
-    imageService: "passthrough"
-  }),
-  server: {
-    headers: {
-      // 👇 `credentialless` is the trick to get both WebContainers & CORS images to both load
-      // See: https://developer.chrome.com/blog/coep-credentialless-origin-trial/#credentialless-to-the-rescue
-      "Cross-Origin-Embedder-Policy": "credentialless",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    }
-  },
+  })]
 });

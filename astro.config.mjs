7d2e1672a58e3a9daa40b68a9e3240d80e515b4d
Josh Rehders <jr8ers@gmail.com>
2025-03-11 23:33:46 -0400

    try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,28 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import cloudflare from '@astrojs/cloudflare';
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rehde.rs",
-  integrations: [
-    tailwind(), 
-    // Remove the image integration or keep it only for local images
-    mdx(), 
-    partytown({
-      config: {
-        debug: true,
-        forward: ['dataLayer.push', 'gtag']
-      }
-    })
-  ],
+  integrations: [tailwind(),
+  // Remove the image integration or keep it only for local images
+  mdx(), partytown({
+    config: {
+      debug: true,
+      forward: ['dataLayer.push', 'gtag']
+    }
+  })],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "passthrough"
+  }),
   server: {
     headers: {
       // 👇 `credentialless` is the trick to get both WebContainers & CORS images to both load
       // See: https://developer.chrome.com/blog/coep-credentialless-origin-trial/#credentialless-to-the-rescue
       "Cross-Origin-Embedder-Policy": "credentialless",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    },
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
   },
 });
